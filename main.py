dae76740887508115454680cd2ad366ecead10d5
kryuger <kryuger7795@yandex.ru>
2025-12-19 15:02:23 +0800

    Создано игровое поле и реализована кнопка выхода из игры

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 import pygame
+
+pygame.init()
+
+dis_width = 800 #Зададим размер игрового поля через две переменные.
+dis_height = 600
+dis = pygame.display.set_mode((dis_width,dis_height))  #Задаём размер игрового поля.
+game_over = False #Переменная которая помогает контролировать - завершена игра или нет
+
+pygame.display.update()
+
+
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: #Если нажата кнопка закрыть 
+            game_over = True # То game_over принимает знаение True
+        print(event) #Выводить в терминал все события
+        
+pygame.quit()
+quit()
